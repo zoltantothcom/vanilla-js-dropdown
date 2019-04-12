3044259fd6563814a90fc7f3cdbf2382e6d3a689
Zoltan Toth <contact@zoltantoth.com>
2019-04-12 14:29:52 -0400

    Update to accommodate optgroups

--- src/javascript/vanilla-js-dropdown.js
+++ src/javascript/vanilla-js-dropdown.js
@@ -1,7 +1,7 @@
 /**
 * @fileOverview
 * @author Zoltan Toth
-* @version 2.1.2
+* @version 2.2.0
 */
 
 /**
@@ -12,131 +12,162 @@
 * @param {(string|Object)} options.elem - HTML id of the select or the DOM element.
 */
 var CustomSelect = function(options) {
-    var elem          = typeof options.elem === 'string' ?
-                          document.getElementById(options.elem) : options.elem,
-        mainClass     = 'js-Dropdown',
-        titleClass    = 'js-Dropdown-title',
-        listClass     = 'js-Dropdown-list',
-        selectedClass = 'is-selected',
-        openClass     = 'is-open',
-        selectOptions = elem.options,
-        optionsLength = selectOptions.length;
+  var elem           = typeof options.elem === 'string' ?
+      document.getElementById(options.elem) : options.elem,
+      mainClass      = 'js-Dropdown',
+      titleClass     = 'js-Dropdown-title',
+      listClass      = 'js-Dropdown-list',
+      optgroupClass  = 'js-Dropdown-optgroup',
+      selectedClass  = 'is-selected',
+      openClass      = 'is-open',
+      selectOpgroups = elem.getElementsByTagName('optgroup'),
+      selectOptions  = elem.options,
+      optionsLength  = selectOptions.length;
 
-    // creating the pseudo-select container
-    var selectContainer = document.createElement('div');
+  // creating the pseudo-select container
+  var selectContainer = document.createElement('div');
 
-    selectContainer.className = mainClass;
+  selectContainer.className = mainClass;
 
-    if (elem.id) {
-      selectContainer.id = 'custom-' + elem.id;
+  if (elem.id) {
+    selectContainer.id = 'custom-' + elem.id;
+  }
+
+  // creating the always visible main button
+  var button = document.createElement('button');
+
+  button.className = titleClass;
+  button.textContent = selectOptions[0].textContent;
+
+  // creating the UL
+  var ul = document.createElement('ul');
+  ul.className = listClass;
+
+  // dealing with optgroups
+  if (selectOpgroups.length) {
+    for (var i = 0; i < selectOpgroups.length; i++) {
+      var div = document.createElement('div');
+      div.innerText = selectOpgroups[i].label;
+      div.classList.add(optgroupClass);
+      
+      ul.appendChild(div);
+      generateOptions(selectOpgroups[i].getElementsByTagName('option'));
     }
+  } else {
+    generateOptions(selectOptions);
+  }
 
-    // creating the always visible main button
-    var button = document.createElement('button');
+  // appending the button and the list
+  selectContainer.appendChild(button);
+  selectContainer.appendChild(ul);
 
-    button.className = titleClass;
-    button.textContent = selectOptions[0].textContent;
+  selectContainer.addEventListener('click', onClick);
 
-    // creating the UL
-    var ul = document.createElement('ul');
-    ul.className = listClass;
+  // pseudo-select is ready - append it and hide the original
+  elem.parentNode.insertBefore(selectContainer, elem);
+  elem.style.display = 'none';
 
-    for (var i = 0; i < optionsLength; i++) {
-        var li = document.createElement('li');
+  /**
+    * Generates a list from passed options.
+    *
+    * @param {object} options - options for the whole select or for an optgroup.
+    */
+  function generateOptions(options) {
+    for (var i = 0; i < options.length; i++) {
+      var li = document.createElement('li');
 
-        li.innerText = selectOptions[i].textContent;
-        li.setAttribute('data-value', selectOptions[i].value);
-        li.setAttribute('data-index', i);
+      li.innerText = options[i].textContent;
+      li.setAttribute('data-value', options[i].value);
+      li.setAttribute('data-index', i);
 
-        if (elem.selectedIndex === i) {
-            li.classList.add(selectedClass);
-            button.textContent = selectOptions[i].textContent;
-        }
+      if (selectOptions[elem.selectedIndex].textContent === options[i].textContent) {
+        li.classList.add(selectedClass);
+        button.textContent = options[i].textContent;
+      }
 
-        ul.appendChild(li);
+      ul.appendChild(li);
     }
+  }
 
-    // appending the button and the list
-    selectContainer.appendChild(button);
-    selectContainer.appendChild(ul);
-
-    selectContainer.addEventListener('click', onClick);
-
-    // pseudo-select is ready - append it and hide the original
-    elem.parentNode.insertBefore(selectContainer, elem);
-    elem.style.display = 'none';
-
-
-    /**
+  /**
     * Closes the current select on any click outside of it.
     *
     */
-    document.addEventListener('click', function(e) {
-        if (!selectContainer.contains(e.target)) close();
-    });
+  document.addEventListener('click', function(e) {
+    if (!selectContainer.contains(e.target)) close();
+  });
 
-    /**
+  /**
     * Handles the clicks on current select.
     *
     * @param {object} e - The item the click occured on.
     */
-    function onClick(e) {
-        e.preventDefault();
+  function onClick(e) {
+    e.preventDefault();
 
-        var t = e.target; // || e.srcElement; - uncomment for IE8
+    var t = e.target; // || e.srcElement; - uncomment for IE8
 
-        if (t.className === titleClass) {
-            toggle();
-        }
-
-        if (t.tagName === 'LI') {
-            selectContainer.querySelector('.' + titleClass).innerText = t.innerText;
-            elem.options.selectedIndex = t.getAttribute('data-index');
-
-            //trigger 'change' event
-            var evt = new CustomEvent('change');
-            elem.dispatchEvent(evt);
-
-            // highlight the selected
-            for (var i = 0; i < optionsLength; i++) {
-                ul.querySelectorAll('li')[i].classList.remove(selectedClass);
-            }
-            t.classList.add(selectedClass);
-
-            close();
-        }
+    if (t.className === titleClass) {
+      toggle();
     }
 
-    /**
+    if (t.tagName === 'LI') {
+      selectContainer.querySelector('.' + titleClass).innerText = t.innerText;
+      elem.options.selectedIndex = t.getAttribute('data-index');
+
+      //trigger 'change' event
+      var evt = new CustomEvent('change');
+      elem.dispatchEvent(evt);
+
+      // highlight the selected
+      for (var i = 0; i < optionsLength; i++) {
+        ul.querySelectorAll('li')[i].classList.remove(selectedClass);
+      }
+      t.classList.add(selectedClass);
+
+      close();
+    }
+  }
+
+  /**
     * Toggles the open/close state of the select on title's clicks.
     *
     * @public
     */
-    function toggle() {
-        ul.classList.toggle(openClass);
-    }
+  function toggle() {
+    ul.classList.toggle(openClass);
+  }
 
-    /**
+  /**
     * Opens the select.
     *
     * @public
     */
-    function open() {
-        ul.classList.add(openClass);
-    }
+  function open() {
+    ul.classList.add(openClass);
+  }
 
-    /**
+  /**
     * Closes the select.
     *
     * @public
     */
-    function close() {
-        ul.classList.remove(openClass);
-    }
+  function close() {
+    ul.classList.remove(openClass);
+  }
 
-    return {
-        toggle: toggle,
-        close:  close,
-        open:   open
-    };
+  return {
+    toggle: toggle,
+    close:  close,
+    open:   open
+  };
 };
+
+
+var select = new CustomSelect({
+  elem: 'select'
+});
+
+var select = new CustomSelect({
+  elem: 'select2'
+});
